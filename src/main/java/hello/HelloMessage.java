1a45a7aa752d1aef9b78e479524d23bd86e6586f
Sreenidhi Sreesha <sreenidhibs@gmail.com>
2014-12-11 23:50:17 -0800

    Working spring reactor sample

---
+++ src/main/java/hello/HelloMessage.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * Created by sreenidhisreesha on 12/11/14.
+ */
+public class HelloMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/hello/Receiver.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import reactor.event.Event;
+import reactor.function.Consumer;
+
+@Service
+class Receiver implements Consumer<Event<Integer>> {
+
+    @Autowired
+    CountDownLatch latch;
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    public void accept(Event<Integer> ev) {
+        QuoteResource quoteResource = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", QuoteResource.class);
+        System.out.println("Quote " + ev.getData() + ": " + quoteResource.getValue().getQuote());
+        latch.countDown();
+    }
+
+}
